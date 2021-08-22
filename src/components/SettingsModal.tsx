5a061a21e61ca115f6373a36df6f28d721ba551a
aleksandr-zheltishev <a@zheltishev.pro>
2021-08-22 21:52:33 +0300

    Add icon button to open settings modal

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -1,12 +1,16 @@
 import React, {useState} from 'react';
 import Modal from "./generic/Modal";
 import Settings from "./Settings";
+import {IconButton} from "./generic/IconButton";
 
 const SettingsModal = () => {
     const [show, setShow] = useState(false);
 
     return (
-        <Modal show={show} content={<Settings/>} onClick={() => setShow(false)}/>
+        <>
+            <IconButton onClick={() => setShow(true)} icon="cog" className="top-right-corner"/>
+            <Modal show={show} content={<Settings/>} onClick={() => setShow(false)}/>
+        </>
     );
 }
 
