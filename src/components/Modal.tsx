405c804bdd8c1e492e2a3fdd551719b5f2fcbdcf
aleksandr-zheltishev <a@zheltishev.pro>
2021-08-22 21:39:29 +0300

    Modal generic component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+type Props = {
+    show: boolean
+    content: React.ReactNode;
+    onClick: () => void;
+}
+
+const Modal = ({show, content, onClick}: Props) => {
+    const component = <div className="overlay">
+        <div className="modal" id="modal">
+            <div className="content">
+                {content}
+            </div>
+            <div className="actions">
+                <button className="toggle-button" onClick={onClick}>
+                    Close
+                </button>
+            </div>
+        </div>
+    </div>;
+
+    return (
+        <>
+            {show ? component : null}
+        </>
+    );
+}
+
+export default Modal;
