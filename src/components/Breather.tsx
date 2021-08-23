9dcf4ded2c90033a374a572ae7bb0d15edd8edec
aleksandr-zheltishev <89268524+aleksandr-zheltishev@users.noreply.github.com>
2021-08-23 22:26:24 +0300

    Smoother animation (#9)

--- src/components/Breather.tsx
+++ src/components/Breather.tsx
@@ -6,22 +6,31 @@ import HorizontalStepper from "./generic/HorizontalStepper";
 import Button from "./generic/Button";
 
 export const Breather = () => {
-    const [minutes, setMinutes] = useState(5)
+    const [minutes, setMinutesState] = useState(+(localStorage.getItem('breathing-minutes') || 5))
     const [isRunning, setRunning] = useState(false)
 
+    const setMinutes = (v: number) => {
+        setMinutesState(v);
+        localStorage.setItem('breathing-minutes', v.toString());
+    }
+
     const increaseMinutes = () => {
-        if (minutes >= 60) {
-            setMinutes(60);
-        } else {
+        if (minutes < 5) {
+            setMinutes(minutes + 1);
+        } else if (minutes < 60) {
             setMinutes(minutes + 5);
+        } else {
+            setMinutes(60);
         }
     }
 
     const decreaseMinutes = () => {
-        if (minutes <= 5) {
-            setMinutes(5)
+        if (minutes <= 1) {
+            setMinutes(1)
+        } else if (minutes <= 5) {
+            setMinutes(minutes - 1);
         } else {
-            setMinutes(minutes - 5);
+            setMinutes(minutes - 5)
         }
     }
 
@@ -31,13 +40,13 @@ export const Breather = () => {
     }
 
     return (
-        <>
+        <div className={isRunning ? 'hide-cursor' : ''}>
             <SvgImage path={flower} className={isRunning ? 'flower flower-static' : 'flower-static'}/>
-            {isRunning ? null : <>
-                <HorizontalStepper value={`${minutes} minutes`} decrease={decreaseMinutes}
+            <div className={isRunning ? 'hidden' : ''}>
+                <HorizontalStepper value={`${minutes} minute${minutes > 1 ? 's' : ''}`} decrease={decreaseMinutes}
                                    increase={increaseMinutes}/>
                 <Button onClick={run} text={"Begin"}/>
-            </>}
-        </>
+            </div>
+        </div>
     );
 };
